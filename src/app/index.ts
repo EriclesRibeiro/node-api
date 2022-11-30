4eeb228689c3d0e0b6381f3945c291652e668180
EriclesRibeiro <ericles00@hotmail.com>
2022-11-30 20:17:38 -0300

    feature: configuração do index e header

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,5 +1,17 @@
-import express from 'express'
+import express from 'express';
+import cors from 'cors';
 
-const app = express()
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use('*', function(req, res, next) {
+    res.setHeader('Access-Control-Allow-Headers', 'x-access-token, Origin, Content-Type, Accept');
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Credentials', 'true');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    next();
+});
 
 export default app;
