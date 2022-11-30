d2a05fda673bf19b30177fca4d54852d39acda9c
EriclesRibeiro <ericles00@hotmail.com>
2022-11-30 20:09:57 -0300

    feature: criação e configuração da porta de acesso

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+import app from './app';
+import 'dotenv/config';
+
+const PORT = process.env.PORT || process.env.LOCAL_PORT;
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}!`));
